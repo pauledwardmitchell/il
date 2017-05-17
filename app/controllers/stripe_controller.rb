35b20968853d07ad7ab246b892cc6aaf16ca9990
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-05-17 09:29:26 -0500

    Remove auth user to debug webhook

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -1,7 +1,6 @@
 class StripeController < ApplicationController
 
   protect_from_forgery :except => :webhook
-  authenticate_user! :except => :webhook
 
   def webhook
     # Capture the event information from the webhook params
@@ -9,10 +8,6 @@ class StripeController < ApplicationController
 
     # Verify that the event isn't forged to your Stripe account
     event = Stripe::Event.retrieve(event_id)
-
-    p event_id
-
-    p event
 
     render status: 200
   end
