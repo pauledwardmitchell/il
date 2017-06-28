f92b1c6493004a73e0af6ce65a7dbc708ef3434d
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-06-28 10:34:42 -0500

    Add basic link box

--- app/assets/javascripts/components/program_little_square.js.jsx
+++ app/assets/javascripts/components/program_little_square.js.jsx
@@ -15,11 +15,30 @@ const ProgramLittleSquare = React.createClass({
         width: 200,
         height: 240
       },
+
+      link: {
+        backgroundColor: 'rgb(34, 34, 34)',
+        backgroundImage: `url('https://s3-us-west-2.amazonaws.com/innerlfie/Photos/cez2.jpeg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        position: 'relative',
+        height: 240,
+        width: 200,
+        color: 'rgb(34, 34, 34)',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        overflow: 'hidden'
+      }
     }
 
     return (
       <section style={styles.outerBox}>
         <section style={styles.innerBox}>
+          <a>
+            <section style={styles.link}>
+            </section>
+          </a>
         </section>
       </section>
 
