cb21110ffffa51c021d7657ed039707d565f46e1
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-07-07 09:24:53 -0500

    Replace inline js

--- app/assets/javascripts/hide_buttons.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  document.getElementsByClassName("stripe-button-el")[0].style.display = 'none';
-  document.getElementsByClassName("stripe-button-el")[1].style.display = 'none';
-  document.getElementsByClassName("stripe-button-el")[2].style.display = 'none';
-})
